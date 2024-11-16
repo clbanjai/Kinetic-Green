cdeba68c7263b9d14900d13ceb42ca104c736094
Kevin Li <26943287+kev556@users.noreply.github.com>
2024-11-16 13:41:26 -0500

    imports json

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-from json import *
+import json
 
 app = Flask(__name__)
 
+# Load GeoJSON data
+with open('json/geo.json') as f:
+    geojson_data = json.load(f)
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('map.html', geojson_data=json.dumps(geojson_data))
 
 if __name__ == '__main__':
     app.run(debug=True)
