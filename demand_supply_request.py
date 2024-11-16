f6dbe63cab79f93e291d64c200376a1de9ce5b25
Scotty Hong <136658939+ScottyHong@users.noreply.github.com>
2024-11-16 22:56:56 +0000

    demanand

--- demand_supply_request.py
+++ demand_supply_request.py
@@ -7,7 +7,7 @@ api_key = "zPamRVTDhbg4KhhEqRRCD6Xod9QWpyyp3geG7uWj"
 
 
 def fetch_demand_data(start_time, end_time,grid_operator="NE", frequency="daily"):
-    url = f"https://api.eia.gov/v2/electricity/rto/daily-region-sub-ba-data/data/?frequency={frequency}&data[0]=value&start={start_time}&end={end_time}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key={api_key}"
+    url = f"https://api.eia.gov/v2/electricity/rto/daily-region-data/data/?frequency=daily&data[0]=value&facets[respondent][]=ERCO&start=2023-01-01&end=2024-11-15&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key={api_key}"
     response = requests.get(url)
     data = response.json()
     data = data['response']['data']
@@ -19,6 +19,7 @@ def fetch_demand_data(start_time, end_time,grid_operator="NE", frequency="daily"
 def fetch_supply_data(start_time,end_time):
     url = f"https://api.eia.gov/v2/electricity/rto/daily-fuel-type-data/data/?frequency=daily&data[0]=value&start={start_time}&end={end_time}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key={api_key}"
     response=requests.get(url)
+    print(response,"AAAA")
     data=response.json()
     data=data['response']['data']
     df=pd.DataFrame(data)
@@ -31,6 +32,6 @@ def annual_capacity_by_state():
     data=data['response']['data']
     df=pd.DataFrame(data)
     df.to_csv("capacity.csv")
-# fetch_demand_data("2024-11-04","2024-11-04")
+    
+fetch_demand_data("2024-01","2024-01",frequency='daily')
 
-annual_capacity_by_state()
