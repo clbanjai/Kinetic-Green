28436d828124773fc6950f436f08fa02395c7994
Kevin Li <26943287+kev556@users.noreply.github.com>
2024-11-16 16:42:08 -0500

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ with open('json/geo.json') as f:
 
 @app.route('/')
 def home():
-    return render_template('map.html', geojson_data=json.dumps(geojson_data))
+    return render_template('index.html', geojson_data=json.dumps(geojson_data))
 
 if __name__ == '__main__':
     app.run(debug=True)
