77372e7694ee9a74360ef8b9a4b1c585dcf69e81
Scotty Hong <136658939+ScottyHong@users.noreply.github.com>
2024-11-16 22:00:16 -0500

    ddahkhjk

--- demand_supply_request.py
+++ demand_supply_request.py
@@ -17,7 +17,7 @@ def fetch_demand_data(start_time, end_time,grid_operator="NE", frequency="daily"
     df.to_csv('Demand.csv')
 
 def fetch_supply_data(start_time,end_time):
-    url = f"https://api.eia.gov/v2/electricity/rto/daily-fuel-type-data/data/?frequency=daily&data[0]=value&start={start_time}&end={end_time}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key={api_key}"
+    url = f"https://api.eia.gov/v2/electricity/rto/daily-region-data/data/?frequency=daily&data[0]=value&facets[respondent][]=ERCO&start=2024-11-01&end=2024-11-16&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key={api_key}"
     response=requests.get(url)
     data=response.json()
     data=data['response']['data']
@@ -31,6 +31,6 @@ def annual_capacity_by_state():
     data=data['response']['data']
     df=pd.DataFrame(data)
     df.to_csv("capacity.csv")
-# fetch_demand_data("2024-11-04","2024-11-04")
+fetch_supply_data("2024-11-04","2024-11-04")
 
 annual_capacity_by_state()
